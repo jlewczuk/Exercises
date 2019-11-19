7e417f3ffef3f7df7e5e0189c42b7a162fb974ff
Jarosław Lewczuk <jar.lewczuk@gmail.com>
2019-11-19 13:43:06 +0100

    new day new code

---
+++ Slide in on scroll/script.js
@@ -0,0 +1,36 @@
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this,
+      args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+    console.log(window.scrollY);
+    sliderImages.forEach(sliderImage => {
+        // half way thru the image
+        const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+        // bottom of the image
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        const inNotScrolledPast = window.scrollY < imageBottom;
+        if (isHalfShown && inNotScrolledPast) {
+            sliderImage.classList.add('active');
+        } else {
+            sliderImage.classList.remove('active');
+        }
+    })
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
